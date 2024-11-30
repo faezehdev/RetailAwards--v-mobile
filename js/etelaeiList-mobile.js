629cd40e06ca6bc1ffcd033c77a00145eb056c28
Zeinab <zeinabansarian6280@gmail.com>
2024-11-30 17:58:33 +0330

    new checklist

--- js/etelaeiList-mobile.js
+++ js/etelaeiList-mobile.js
@@ -1,41 +1,51 @@
-ScrollTrigger.create({
-    trigger: '.pinSec',
-    start: 'top top',
-    endTrigger: ".listcontents",
-    end: "bottom bottom",
-    pin: true,
-    pinSpacing:false,
-  })
-  let titleMain = document.querySelectorAll('.Title-main')
-titleMain.forEach(t=>{
-    gsap.to(t.querySelector('h1'),{
-        y:0,
-        scrollTriggers:{
-          trigger:t,
-          start:'top 60%',
-          end:'bottom bottom'
-        }
-      })
-    gsap.to(t.querySelector('.line'),{
-      width:'60px',
-      scrollTriggers:{
-        trigger:t,
-        start:'top 60%',
-        end:'bottom bottom'
-      }
-    })
-})
-let EtalaeiItem = document.querySelectorAll('.EtalaeiItem')
-EtalaeiItem.forEach(e=>{
-    gsap.to(e,{
-      opacity:1,
-      y:0,
-      stagger: 0.5,
-        scrollTriggers:{
-          trigger:e,
-          start:'top 80%',
-          end:'bottom bottom',
-         
-        }
-      })
-})
+gsap.registerPlugin(ScrollTrigger)
+// ScrollTrigger.create({
+//   trigger: ".pinSec",
+//   start: "top top",
+//   endTrigger: ".listcontents",
+//   end: "bottom bottom",
+//   pin: true,
+//   pinSpacing: false,
+// });
+let titleMain = document.querySelectorAll(".Title-main");
+titleMain.forEach((t) => {
+  gsap.to(t.querySelector("h1"), {
+    y: 0,
+    scrollTrigger: {
+      trigger: t,
+      start: "top 60%",
+      end: "bottom bottom",
+    },
+  });
+  gsap.to(t.querySelector(".line"), {
+    width: "60px",
+    scrollTrigger: {
+      trigger: t,
+      start: "top 60%",
+      end: "bottom bottom",
+    },
+  });
+});
+let EtalaeiItem = document.querySelectorAll(".EtalaeiItem");
+EtalaeiItem.forEach((e) => {
+  gsap.to(e, {
+    opacity: 1,
+    y: 0,
+    stagger: 0.5,
+    scrollTrigger: {
+      trigger: e,
+      start: "top 90%",
+      end: "bottom bottom",
+    },
+  });
+});
+
+
+
+
+
+
+
+
+
+
